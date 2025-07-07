04dbc500c8ee5a8364a2018f1689e54616c21c60
SaidPM <said2005marquez@gmail.com>
2025-07-06 20:03:13 -0500

    pantalla succes creada

---
+++ chapp/lib/features/home/presentation/succesview.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class SuccesV extends StatelessWidget {
+  const SuccesV({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      backgroundColor: Colors.black,
+      centerTitle: true,
+      title: const Text('Respuesta de la IA', style: TextStyle(color: Colors.white),),
+    ),
+    body: Stack(
+      children: [
+        Positioned.fill(
+          child: Image.network(
+            'https://static.vecteezy.com/system/resources/previews/017/439/206/non_2x/abstract-background-design-background-texture-design-in-abstract-style-creative-illustration-for-advertising-posters-business-cards-flyers-websites-banners-covers-landings-pages-etc-vector.jpg',
+            fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) => Container(color: Colors.grey),
+          ),
+        ),
+        SafeArea(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16.0),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: MediaQuery.of(context).size.height - kToolbarHeight - MediaQuery.of(context).padding.top,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 20),
+                  Image.network(
+                    'https://cdn-icons-png.flaticon.com/128/9427/9427117.png',
+                    height: 100,
+                    errorBuilder: (context, error, stackTrace) => const Icon(Icons.error, size: 100, color: Colors.red),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Respuesta recibida',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.9),
+                      borderRadius: BorderRadius.circular(12.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 8,
+                          offset: const Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    
+                    ),
+                  
+                  const SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+    );
+  }
+}
